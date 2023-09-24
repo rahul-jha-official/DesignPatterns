805c451ac7da34956b7ce4854176e22f0465aa05
rahul-jha-official <rahul-jha-official@outlook.com>
2023-09-24 14:11:07 +0530

    Single Responsibility Principle

---
+++ SOLIDPrinciples/SingleResponsibilityPrinciple/DemoWithSRP.cs
@@ -0,0 +1,41 @@
+﻿namespace SOLIDPrinciples.SingleResponsibilityPrinciple;
+/// <summary>
+/// Here, 
+/// Journal class having single responsibility of updating journal
+/// Persistence class having responsibility of saving journal to file.
+/// </summary>
+public class Journal
+{
+    private readonly IList<string> entries;
+
+    public Journal()
+    {
+        entries = new List<string>();
+    }
+
+    public void Add(string entry)
+    {
+        entries.Add(entry);
+    }
+
+    public void Remove(int index)
+    {
+        entries.RemoveAt(index);
+    }
+
+    public override string? ToString()
+    {
+        return string.Join(Environment.NewLine, entries.Select((e, i) => $"{(i + 1)}\t{e}"));
+    }
+}
+
+public class Persistence
+{
+    public void Save(Journal journal, string filename, bool overwrite = false)
+    {
+        if (overwrite || !File.Exists(filename))
+        {
+            File.WriteAllText(filename, journal.ToString());
+        }
+    }
+}
---
+++ SOLIDPrinciples/SingleResponsibilityPrinciple/DemoWithoutSRP.cs
@@ -0,0 +1,37 @@
+﻿namespace SOLIDPrinciples.SingleResponsibilityPrinciple;
+
+/// <summary>
+/// Here we are violating Single Responsibility Principle as we have given too many responsibity to single class.
+/// </summary>
+public class Journal_WO_SRP
+{
+    private readonly IList<string> entries;
+
+    public Journal_WO_SRP()
+    {
+        entries = new List<string>();
+    }
+
+    public void Add(string entry)
+    {
+        entries.Add(entry);
+    }
+
+    public void Remove(int index)
+    {
+        entries.RemoveAt(index);
+    }
+
+    public override string? ToString()
+    {
+        return string.Join(Environment.NewLine, entries.Select((e, i) => $"{(i + 1)}\t{e}"));
+    }
+
+    public void Save(string filename, bool overwrite = false)
+    {
+        if (overwrite || !File.Exists(filename))
+        {
+            File.WriteAllText(filename, ToString());
+        }
+    }
+}
